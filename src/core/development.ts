17393df337f9aeb66bcd562abe1b9d6e948750b8
Max Plotitsyn <karrtopelka@gmail.com>
2025-05-12 19:19:33 +0300

    init 3

--- src/core/development.ts
+++ src/core/development.ts
@@ -8,7 +8,7 @@ const development = async (bot: Telegraf<Context<Update>>) => {
   const botInfo = (await bot.telegram.getMe()).username;
 
   debug('Bot runs in development mode!');
-  debug(`${botInfo} deleting webhook`);
+  debug(`${botInfo} deleting webhook :)`);
   await bot.telegram.deleteWebhook();
   debug(`${botInfo} starting polling`);
 
