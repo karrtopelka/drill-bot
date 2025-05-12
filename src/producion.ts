1d47d02d28ca253c1aabb8cd70314de8d1120178
Max Plotitsyn <karrtopelka@gmail.com>
2025-05-12 22:29:36 +0300

    [ADD] production

---
+++ src/producion.ts
@@ -0,0 +1,16 @@
+import { bot } from './bot';
+import debug from 'debug';
+
+const debugLog = debug('bot:production');
+
+// Start the bot in long-polling mode
+debugLog('Bot is starting in production mode (polling)...');
+bot.start({
+  onStart: (botInfo) => {
+    debugLog(`Bot @${botInfo.username} started in production mode!`);
+  },
+});
+
+// Setup graceful stop
+process.once('SIGINT', () => bot.stop());
+process.once('SIGTERM', () => bot.stop());
